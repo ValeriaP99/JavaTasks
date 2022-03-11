0d463dee1d72d483a215ec31eefffc959b81ce83
ValeriaP99 <popescu.valeria1@usm.md>
2022-03-11 19:40:34 +0200

    Commit chapter6

--- src/md/pentalog/tasks_part1/chapter6/Ex21.java
+++ src/md/pentalog/tasks_part1/chapter6/Ex21.java
@@ -4,13 +4,6 @@ public class Ex21 {
     public static void main(String[] args) {
         System.out.println("-----[ Display digits ]--------");
         displayDigits(3456);
-
-        int n = 10;
-        for (int i = 0; i < 4; i++){
-            n *= 10;
-        }
-        System.out.println(n);
-
     }
 
     public static int calculateIntegerPart(int a, int b){
@@ -32,18 +25,14 @@ public class Ex21 {
 
     public static void displayDigits(int n){
         int length = countLength(n);
-        int ten = 10;
+        int ten;
         int num;
-
-
-
-        /*while (n != 0){
-            ten = (ten*length)/length;
+        ten = (int) Math.pow(10, (length-1));
+        while (n != 0){
             num = calculateIntegerPart(n, ten);
             n = calculateRemainder(n, ten);
             System.out.print(num+"  ");
             ten /= 10;
-            //length -= 1;
-        }*/
+        }
     }
 }
--- src/md/pentalog/tasks_part1/chapter6/Ex31.java
+++ src/md/pentalog/tasks_part1/chapter6/Ex31.java
@@ -1,4 +1,64 @@
 package md.pentalog.tasks_part1.chapter6;
 
+import java.security.SecureRandom;
+import java.util.Scanner;
+
 public class Ex31 {
+    public static void main(String[] args) {
+        SecureRandom random = new SecureRandom();
+        int c;
+        int rand;
+        int num;
+        int count = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("-----------------------");
+        System.out.println("|   Start   |   Quit   |");
+        System.out.println("-----------------------");
+        System.out.println("|     1     |     2    |");
+        System.out.println("-----------------------");
+        System.out.print("Your choice: ");
+        c = scanner.nextInt();
+
+        while (c == 1){
+            System.out.print("Enter a number ");
+            num =  scanner.nextInt();
+            rand = random.nextInt(1000);
+            System.out.println("Random number  = "+rand);
+            count += 1;
+
+            while(num != rand){
+                if(num > rand){
+                    System.out.println("Too high");
+                    System.out.print("Enter a number ");
+                    num =  scanner.nextInt();
+                    count += 1;
+                }else if(num < rand){
+                    System.out.println("Too low");
+                    System.out.print("Enter a number ");
+                    num =  scanner.nextInt();
+                    count += 1;
+                }
+            }
+
+            System.out.println("*************[ BINGO ]*************");
+            System.out.printf("| Total guesses : %d               |%n", count);
+            if(count <= 10){
+                System.out.println("| You got lucky !!!               |");
+            }else if(count > 10){
+                System.out.println("| You should be able to do better |");
+            }
+            System.out.println("************************************");
+
+            System.out.println();
+            System.out.println("-----------------------");
+            System.out.println("|   Start   |   Quit   |");
+            System.out.println("-----------------------");
+            System.out.println("|     1     |     2    |");
+            System.out.println("-----------------------");
+            System.out.print("Your choice: ");
+            c = scanner.nextInt();
+        }
+    }
 }
