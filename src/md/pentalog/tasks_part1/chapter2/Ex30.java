0cfdbbe3b10a59e0016ea80130b312c585223b6f
ValeriaP99 <popescu.valeria1@usm.md>
2022-03-16 17:30:44 +0200

    Modify Ex30 - chapter2

--- src/md/pentalog/tasks_part1/chapter2/Ex30.java
+++ src/md/pentalog/tasks_part1/chapter2/Ex30.java
@@ -16,7 +16,11 @@ public class Ex30 {
             clone /= 10;
             i++;
         }
-        if(i == 5){
+        if(i < 5){
+            System.out.println("The number is short, enter a five-digit number");
+        }else if(i > 5){
+            System.out.println("The number is long, enter a five-digit number");
+        } else {
             int d1 = number / 10000;
             number %= 10000;
             int d2 = number / 1000;
@@ -28,10 +32,6 @@ public class Ex30 {
             int d5 = number;
 
             System.out.println(d1+"   "+d2+"   "+d3+"   "+d4+"   "+d5);
-        }else if(i < 5){
-            System.out.println("The number is short, enter a five-digit number");
-        }else if(i > 5){
-            System.out.println("The number is long, enter a five-digit number");
         }
     }
 }
