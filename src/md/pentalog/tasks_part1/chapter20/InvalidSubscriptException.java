de783a30d17bf63f2da7c9a2b83310c8e8931c78
ValeriaP99 <popescu.valeria1@usm.md>
2022-04-04 14:57:47 +0300

    Commit chapter20

---
+++ src/md/pentalog/tasks_part1/chapter20/InvalidSubscriptException.java
@@ -0,0 +1,7 @@
+package md.pentalog.tasks_part1.chapter20;
+
+public class InvalidSubscriptException extends Exception{
+    public InvalidSubscriptException(String message) {
+        super(message);
+    }
+}
