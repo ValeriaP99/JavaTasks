c15fcfed0c1773d02bc0526fc9049c6a2a3a0e86
ValeriaP99 <popescu.valeria1@usm.md>
2022-04-12 15:09:15 +0300

    Commit chapter14

--- src/md/pentalog/tasks_part1/chapter14/Ex14_11.java
+++ src/md/pentalog/tasks_part1/chapter14/Ex14_11.java
@@ -12,18 +12,27 @@ public class Ex14_11 {
 
         System.out.println("Enter a line a line of text");
         text = scanner.nextLine();
+
         System.out.print("Enter a search character -> ");
         charI = scanner.next().charAt(0);
 
         int count = 0;
+        boolean bool = true;
         int index = 0;
 
-        while(index != -1){
-            index = text.indexOf(charI, index);
-            if(index != -1){
+        while(bool){
+
+            int index2 = text.indexOf(charI, index);
+            //text = text.substring(index);
+
+            if(index2 != -1){
+                bool = true;
                 count += 1;
+                System.out.println("Letter "+charI+" is located at index "+(index2));
+                index = index2+1;
+            }else{
+                bool = false;
             }
-            System.out.println("Letter "+charI+" is located at index "+(index));
         }
 
         System.out.println("Count = "+count);
